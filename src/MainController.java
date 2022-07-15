ebc18ffaccaa784752d40684324116ae0f6e0cb4
Christopher Perez <cfperez97@gmail.com>
2022-07-15 14:55:42 -0400

    First Commit

---
+++ src/MainController.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+public class MainController {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+    }
+
+}
