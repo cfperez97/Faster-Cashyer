5dca7a51477b642dcc70b3a16d2461a31dde0745
Christopher Perez <cfperez97@gmail.com>
2022-07-15 21:48:50 -0400

    Added Java files for objects

---
+++ src/CashierWindow.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class CashierWindow {
+
+    public static void createWindow() {
+
+        // create and set up the window
+        JFrame frame = new JFrame("Simple GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel textLabel = new JLabel("I'm a label in the window",SwingConstants.CENTER);
+        textLabel.setPreferredSize(new Dimension(300, 100));
+        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+
+        //Create and set up the window.
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+}
--- src/MainController.java
+++ src/MainController.java
@@ -1,13 +1,11 @@
-import java.awt.*;
-import javax.swing.*;
-
 
 public class MainController {
 
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        CashierWindow.createWindow();
+        CustomerWindow.createWindow();
 
     }
-
 }
